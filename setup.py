a036d4073fcdfae4c2c13686efafb1ba5c14f3cf
Matthew Pounsett <matt@conundrum.com>
2022-02-02 08:00:08 -0500

    fixing development status classifier

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     license="Apache Software License 2.0",
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Environment :: Console',
